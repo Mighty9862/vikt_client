070906961e79a5106dc04cce06dab4dd436252aa
Mighty9862 <admin@admin.local>
2025-02-25 15:26:55 +0300

    New Rating

--- src/app/pages/Rating/Rating.jsx
+++ src/app/pages/Rating/Rating.jsx
@@ -1,33 +1,24 @@
 import styles from "./Rating.module.scss";
 import Table from "../../components/table/Table";
 import { useNavigate } from "react-router-dom";
-import { useRef, useEffect } from "react";
 
 function Rating() {
   const navigate = useNavigate();
-  const wsRef = useRef(null);
 
-  useEffect(() => {
-    if (!wsRef.current) {
-      wsRef.current = new WebSocket(
-        "ws://80.253.19.93:8000/api/v2/websocket/ws/spectator"
-      );
+  const ws = new WebSocket(
+    "ws://80.253.19.93:8000/api/v2/websocket/ws/spectator"
+  );
 
-      wsRef.current.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        if (data.type === "question") {
-          navigate("/projector", { state: { data: data } });
-        }
-      };
+  function updateDisplay(data) {
+    if (data.type === "question") {
+      navigate("/projector", { state: { data: data } });
     }
+  }
 
-    return () => {
-      if (wsRef.current) {
-        wsRef.current.close();
-        wsRef.current = null;
-      }
-    };
-  }, [navigate]);
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    updateDisplay(data);
+  };
 
   return (
     <div className={styles.window}>
