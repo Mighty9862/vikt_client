cd2f3a5148ebda03c9941a5f0f5635c748b0760c
Mighty9862 <admin@admin.local>
2025-03-14 12:25:55 +0300

    New IP

--- src/app/pages/Admin/Admin.jsx
+++ src/app/pages/Admin/Admin.jsx
@@ -1,41 +1,45 @@
 import styles from "./Admin.module.scss";
-import AnswerTimer from "../../components/answerTimer/AnswerTimer";
-import { useState } from "react";
 import Button from "../../components/button/Button";
+import { useState } from "react";
+import { m } from "framer-motion";
 
 function Admin() {
+  const [count, setCount] = useState(0);
+
   function startGame() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/start", {
+    fetch("http://10.10.0.88:8000/api/v2/websocket/admin/start", {
       method: "POST",
     });
   }
   function nextQuestion() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/next", {
+    fetch("http:/10.10.0.88:8000/api/v2/websocket/admin/next", {
       method: "POST",
     });
+
+    setCount((count) => count + 1);
   }
   function stopGame() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/stop", {
+    fetch("http://10.10.0.88:8000/api/v2/websocket/admin/stop", {
       method: "POST",
     });
   }
   function showRating() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/show_rating", {
+    fetch("http://10.10.0.88:8000/api/v2/websocket/admin/show_rating", {
       method: "POST",
     });
   }
   function showQuestion() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/show_question", {
+    fetch("http://10.10.0.88:8000/api/v2/websocket/admin/show_question", {
       method: "POST",
     });
   }
   function showAnswers() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/show_answer", {
+    fetch("http://10.10.0.88:8000/api/v2/websocket/admin/show_answer", {
       method: "POST",
     });
   }
   function startTimer() {
-    fetch("http://80.253.19.93:8000/api/v2/websocket/admin/start_timer", {
+    fetch("http://10.10.0.88:8000/api/v2/websocket/admin/start_timer", {
       method: "POST",
     });
   }
@@ -48,11 +52,30 @@ function Admin() {
     <div className={styles.window}>
       <title>Викторина | Панель администратора</title>
 
+      <div className={styles.rating}>
+        <p className={styles.count}>Номер вопроса: {count}</p>
+        <m.button
+          onClick={() => setCount((count) => count + 1)}
+          className={styles.count__btn}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 1 }}
+        >
+          +
+        </m.button>
+      </div>
+
       {/* Выбор вопроса */}
       <div className={styles.question}>
         <h1>Выбор вопроса</h1>
         <Button onClick={startGame}>Начать игру</Button>
-        <Button onClick={nextQuestion}>Следующий вопрос</Button>
+        <m.button
+          className={styles.next}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 1 }}
+          onClick={nextQuestion}
+        >
+          Следующий вопрос
+        </m.button>
         <Button onClick={stopGame}>Закончить игру</Button>
       </div>
 
@@ -60,7 +83,14 @@ function Admin() {
       <div className={styles.projector}>
         <h1>Проектор</h1>
         <Button onClick={showScreenSaver}>Показать заставку</Button>
-        <Button onClick={showQuestion}>Показать вопрос</Button>
+        <m.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 1 }}
+          className={styles.next}
+          onClick={showQuestion}
+        >
+          Показать вопрос
+        </m.button>
         <Button onClick={showRating}>Показать рейтинг</Button>
         <Button onClick={showAnswers}>Показать ответ на вопрос</Button>
       </div>
