a995d6406654431397febaa1ed8d7654a733c0c0
Mighty9862 <admin@admin.local>
2025-06-04 16:50:19 +0300

    New front

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://10.10.0.88:8000/api/v2",
+  baseURL: "http://80.253.19.93:8000/api/v2",
   withCredentials: false,
 });
 
-// Добавляем токен к каждому запросу
 instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("accessToken");
